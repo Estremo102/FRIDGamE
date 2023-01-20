96bfc4effb9ce490bfb5ff0860700fa9dc84da88
Estremo102 <dominikmozdzen@gmail.com>
2023-01-20 16:10:42 +0100

    news - model

---
+++ Models/News.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FRIDGamE.Models
+{
+    public class News
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime AddDate { get; set; }
+        public string Headline { get; set; }
+        public string Content { get; set; }
+    }
+}
