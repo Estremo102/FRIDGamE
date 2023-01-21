656841879a624e2981497ff7a7ca9eab54fdab4f
Estremo102 <dominikmozdzen@gmail.com>
2023-01-21 17:15:18 +0100

    gamesController

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FRIDGamE.Areas.Identity.Data;
+using FRIDGamE.Models;
+
+namespace FRIDGamE.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly IdentityContext _context;
+
+        public GamesController(IdentityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Games
+        public async Task<IActionResult> Index()
+        {
+            var identityContext = _context.Games.Include(g => g.GamePublisher).Include(g => g.Studio);
+            return View(await identityContext.ToListAsync());
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Games
+                .Include(g => g.GamePublisher)
+                .Include(g => g.Studio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            ViewData["GamePublisherId"] = new SelectList(_context.Publishers, "Id", "PublisherName");
+            ViewData["StudioId"] = new SelectList(_context.Developers, "Id", "DeveloperName");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GameName,StudioId,GamePublisherId,RegularPrice,ReleaseDate")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GamePublisherId"] = new SelectList(_context.Publishers, "Id", "PublisherName", game.GamePublisherId);
+            ViewData["StudioId"] = new SelectList(_context.Developers, "Id", "DeveloperName", game.StudioId);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Games.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            ViewData["GamePublisherId"] = new SelectList(_context.Publishers, "Id", "PublisherName", game.GamePublisherId);
+            ViewData["StudioId"] = new SelectList(_context.Developers, "Id", "DeveloperName", game.StudioId);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GameName,StudioId,GamePublisherId,RegularPrice,ReleaseDate")] Game game)
+        {
+            if (id != game.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GamePublisherId"] = new SelectList(_context.Publishers, "Id", "PublisherName", game.GamePublisherId);
+            ViewData["StudioId"] = new SelectList(_context.Developers, "Id", "DeveloperName", game.StudioId);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Games
+                .Include(g => g.GamePublisher)
+                .Include(g => g.Studio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Games == null)
+            {
+                return Problem("Entity set 'IdentityContext.Games'  is null.");
+            }
+            var game = await _context.Games.FindAsync(id);
+            if (game != null)
+            {
+                _context.Games.Remove(game);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+          return _context.Games.Any(e => e.Id == id);
+        }
+    }
+}
