b6daf1b547796c32a2ec8edfef0ca80eb8f2921a
Estremo102 <dominikmozdzen@gmail.com>
2023-01-22 16:11:12 +0100

    aktualności index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,37 @@
-﻿using FRIDGamE.Models;
+﻿using FRIDGamE.Areas.Identity.Data;
+using FRIDGamE.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 using System.Diagnostics;
 
 namespace FRIDGamE.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly IdentityContext _context;
+
+        public HomeController(IdentityContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        // GET: News
         public IActionResult Index()
         {
-            return View();
+            var identityContext = _context.News.Include(n => n.Author);
+            return View(identityContext.ToList());
         }
-
         public IActionResult Privacy()
         {
             return View();
